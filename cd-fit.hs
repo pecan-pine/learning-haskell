6f4bf50055171978893d6dfb2cfac1bb2c929668
uriel <ebensonnn32@gmail.com>
2020-05-03 16:45:20 -0700

    committing...

--- cd-fit.hs
+++ cd-fit.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Text.ParserCombinators.Parsec
+import Data.List(sortBy)
 
 --parses output of "du -subdirectory"
 parseInput = 
@@ -10,7 +11,7 @@ parseInput =
 
 --datatype Dir holds information about a single directory
 -- its size and name 
-data Dir = Dir Int String deriving Show
+data Dir = Dir {dir_size::Int, dir_name::String} deriving Show
 
 -- `dirAndSize` parses information about single directory, which is:
 -- a size in bytes (number), some spaces, then directory name, which extends till newline
@@ -19,6 +20,28 @@ dirAndSize =
        spaces
        dir_name <- anyChar `manyTill` newline
        return (Dir (read size) dir_name)
+
+--import Data.List(sortBy)
+
+--DirPack holds a set of directories which are to be stored on a CD
+--'pack_size' is stored separately
+data DirPack = DirPack {pack_size::Int, dirs::[Dir]} deriving Show
+
+--for simplicity, assume 700mb CD's
+media_size = 700*1024*1024
+
+--Greedy packer tries to add directories one by one to the initially empty 'DirPack'
+greedy_pack dirs = foldl maybe_add_dir (DirPack 0 []) $ sortBy cmpSize dirs
+    where
+    cmpSize d1 d2 = compare (dir_size d1) (dir_size d2)
+
+--helper function to add directory "d" to the pack "p" when
+--new size doesn't exceed media_size
+maybe_add_dir p d = 
+    let new_size = pack_size p + dir_size d
+        new_dirs = d:(dirs p)
+        in if new_size > media_size then p else DirPack new_size new_dirs
+
 
 
 main = do input <- getContents
@@ -29,3 +52,4 @@ main = do input <- getContents
                                               "\nError:\n" ++ show err
                           Right result -> result
           putStrLn "DEBUG: parsed:"; print dirs
+          putStrLn "Solution:" ; print (greedy_pack dirs)
