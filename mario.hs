7ec001bdde1e813266cb543885049f97f300dee3
uriel <ebensonnn32@gmail.com>
2020-05-03 22:58:18 -0700

    final version of mario.hs probably

--- mario.hs
+++ mario.hs
@@ -5,12 +5,18 @@ import Data.List
 --draw :: Int -> IO ()
 --draw top row as nothing
 draw 0 n = putStr ""
---draw top rows (n will always be the height)
+--draw rows (n will always be the height)
 draw i n = do
     draw (i-1) n
-    putStr $ intercalate "" $ replicate (n-i) " "
-    putStrLn $ intercalate "" $ replicate i "#"
-    
+    rstring (n-i) " "
+    rstring i "#"
+    putStr "  "
+    rstringLn i "#"
+
+--print string s repeated n times
+rstring n s = putStr $ intercalate "" $ replicate n s
+
+rstringLn n s = putStrLn $ intercalate "" $ replicate n s
 
 main = do
     putStrLn "Height:"
