558f58f7a3eeb143e2e8bb56b72f24ff715784ac
uriel <ebensonnn32@gmail.com>
2020-05-03 22:51:15 -0700

    committingggg

--- mario.hs
+++ mario.hs
@@ -3,14 +3,17 @@ module Main where
 import Data.List
 
 --draw :: Int -> IO ()
-draw 1 = putStrLn "#"
-draw n = do
-    draw(n-1)
-    putStrLn $ intercalate "" $ replicate n "#"
+--draw top row as nothing
+draw 0 n = putStr ""
+--draw top rows (n will always be the height)
+draw i n = do
+    draw (i-1) n
+    putStr $ intercalate "" $ replicate (n-i) " "
+    putStrLn $ intercalate "" $ replicate i "#"
     
 
 main = do
     putStrLn "Height:"
     input <- getLine
     let height = read input :: Int
-    draw height 
+    draw height height
